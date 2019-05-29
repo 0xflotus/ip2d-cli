dd3d653dc210eeb02e671149c44ea4be72223152
0xflotus <0xflotus@gmail.com>
2019-05-29 17:14:14 +0200

    prototype

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-var x;
+const { from_str, to_str } = require("ip2d");
+const argv = process.argv.slice(2);
+
+console.log(
+  argv.indexOf("--ip") > -1
+    ? from_str(argv[argv.indexOf("--ip") + 1])
+    : to_str(parseInt(argv[0], 10))
+);
